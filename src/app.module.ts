e0153fc02154a5b1fd049e63e3f4527d1e8bd687
tridat2004 <nguyentridat2k4@gmail.com>
2025-09-24 14:27:56 +0700

    Add file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongoModule } from './database/mongo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // đọc .env toàn project
+    MongoModule,
+  ],
 })
 export class AppModule {}
