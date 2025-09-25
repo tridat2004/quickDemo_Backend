f6ae318011aedb50d7b3dabd46bd27cd9eb7783e
nguyenxuangiang22012004 <nguyenxuangiang22012004@gmail.com>
2025-09-25 16:59:02 +0700

    Fixx

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -3,35 +3,14 @@ import { ArticlesService } from './articles.service';
 import { FilterArticlesDto } from './dto/filter-articles.dto';
 import { filter } from 'rxjs';
 
-interface TotalResponse{
-  total: number;
-}
-interface ArticlesResponse{
-  articles: any[];
-  total: number;
-  page: number;
-  limit: number;
-}
 @Controller('crawl_data')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Get('filter')
-  async getFiltered(
-      @Query(ValidationPipe) filterDto: FilterArticlesDto,
-      @Query('page') page: number = 1,
-      @Query('limit') limit: number = 6,
-    ): Promise<{
-      articles: any[];
-      total: number;
-      page: number;
-      limit: number;
-      totalPages: number;
-      hasNextPage: boolean;
-      hasPreviousPage: boolean;
-    }> {
-      return this.articlesService.findFiltered({ ...filterDto, page, limit });
-}
+  async getFiltered(@Query(ValidationPipe) filterDto: FilterArticlesDto) {
+    return this.articlesService.findFiltered(filterDto);
+  }
 
   @Get('test')
   async testConnection() {
@@ -43,11 +22,12 @@ export class ArticlesController {
     return this.articlesService.getCategories();
   }
 
+  
   @Get('total')
-  async getTotal(): Promise<{total: number}>{
+  async getTotal() {
     return this.articlesService.getTotalArticles();
   }
-  
+
   @Get(`search`)
   async searchArticles(@Query(ValidationPipe) filterDto: FilterArticlesDto) {
     return this.articlesService.findFiltered(filterDto);
